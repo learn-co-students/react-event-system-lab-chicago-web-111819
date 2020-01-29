09f7e295441dc50b929cfe0991296067458f2d91
lluisguardiola <lluisguardiola.h@gmail.com>
2020-01-29 17:05:58 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+
+export default class EyesOnMe extends Component {
+
+    logMessage1 = () => {
+        console.log('Good!')
+    }
+
+    logMessage2 = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render(){
+        return (
+            <button onFocus={this.logMessage1} onBlur={this.logMessage2}> CLICK HERE
+            </button>
+        )
+    }
+}
